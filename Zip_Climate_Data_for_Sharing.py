9dd00ea2ba4bbbbdde2baebe3c1648aaa8353681
DrBenSmith <bensmith1@msn.com>
2022-11-01 12:28:05 +0000

    Additional statements added for adding file that were incomplete in previous run. These incomplete files were manually remade via the 2_bias_correction_quantile_mapping.py script.

--- Zip_Climate_Data_for_Sharing.py
+++ Zip_Climate_Data_for_Sharing.py
@@ -13,13 +13,18 @@
 import os
 from zipfile import ZipFile, ZIP_DEFLATED
 import sys
-
+import pandas as pd
 # import tarfile
 
 root = "I:/SHETRAN_GB_2021/"
-
-zipfolder = root + "05_Climate_Change_Simulations/UKCP18rcm_220708_APM_UK/Bias Controlled Climate Zipfiles/"
 CC = root + "05_Climate_Change_Simulations/UKCP18rcm_220708_APM_UK/"
+zipfolder = CC + "/Bias Controlled Climate Zipfiles/"
+
+# Second pass - path to additional files for zipping:
+only_zip_some_folders = True
+if only_zip_some_folders:
+    files_for_zip = pd.read_csv(zipfolder + "Missing_Climate_Files_for_Copying.csv")
+
 # models = ["01"]  # ["01", '04', "05", '06', '07', '08', '09', '10', '11', '12', '13', '15']
 models = [sys.argv[1]]  # For running via command line, add the model name after the script name.
 
@@ -39,16 +44,21 @@ for m in models:
 
     # List the subfolders top run through:
     master_folder = CC + "bc_" + m + "/"
-    folders = os.listdir(master_folder)
 
-    # Remove any files from the list of folders:
-    folders = [f for f in folders if "." not in f]
+    # If you are only zipping some folders, take these from the list:
+    if only_zip_some_folders:
+        folders = files_for_zip.loc[files_for_zip['RCP'] == int(m)]["Catchment"]
+
+    # Else list them from the available folders in the directory:
+    else:
+        folders = os.listdir(master_folder)
+        # Remove any files from the list of folders:
+        folders = [f for f in folders if "." not in f]
 
     # Run through subfolders (i.e. catchments):
-
     for folder in folders:
 
-        print(m, "-",folder)
+        print(m, "-", folder)
 
         # Set up a zipfile for holding the climate data for that catchment:
         with ZipFile(master_zip + "/" + folder + '.zip', 'w', ZIP_DEFLATED) as zip:
@@ -74,6 +84,6 @@ for m in models:
                     # tar.add(clim_file)
 
     print(catchment_issues)
-    with open(zipfolder + m + '_climate_data_summary.txt', 'w') as f:
+    with open(zipfolder + m + '_climate_data_summary_second_pass.txt', 'w') as f:
         for i in catchment_issues:
             f.write(i)
